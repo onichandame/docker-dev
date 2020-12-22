01134b11b2783faa22398e57c471065626ccad17
onichandame <zxinmyth@gmail.com>
2020-12-22 03:09:38 +0000

    put off sshd as high latency of webssh

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ chmod -R 0700 /root/.ssh
 
 /usr/sbin/sshd
 
+#env | grep _ >> /etc/security/pam_env.conf
+#pip3 install webssh
+#wssh --port=3000 --origin="https://work.onichandame.com" --delay=6 --xheaders=False
+
 dockerd-entrypoint.sh $@
